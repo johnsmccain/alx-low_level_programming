f23d721a302a3efb688a6dc737b89c3753f6e28b
John Danlami <johnsdanlami@gmail.com>
2022-03-10 06:54:15 -0800

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*print text with printf*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/*always return zero */
+	return (0);
+}
