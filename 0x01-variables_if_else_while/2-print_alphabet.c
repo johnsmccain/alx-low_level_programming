8781d6c9654e80b5a161ae3bb8914ffd86265582
John Danlami <johnsdanlami@gmail.com>
2022-03-11 11:32:37 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point 
+ *
+ * function print all low caps
+ * Return 0
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z': low++)
+		putchar(low);
+		putchar('\n');
+	
+	return (0);
+}
