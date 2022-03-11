95ac85e40d5da909ea0480a3a902b9a5e2756c68
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-03-11 22:02:06 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*print text with printf*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	/*always return zero */
 	return (0);
 }
