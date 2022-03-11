5a187856d8826689447248ba5c40465f10a0f2a3
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-03-11 21:54:20 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,22 +10,26 @@
 
 int main(void)
 {
-	int d, p;
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
 		{
-			if (p != d)
+			for (q = p + 1; q <= '9'; q++)
 			{
-				putchar(d);
-				putchar(p);
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
 
-				if (d == '8' && p == '9')
-					continue;
+					if (d == '7' && p == '8')
+						continue;
 
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
