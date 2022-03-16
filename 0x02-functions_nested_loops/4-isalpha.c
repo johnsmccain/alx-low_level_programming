36330baa7bff4863d7985c893236c9af85203312
John Danlami <johnsdanlami@gmail.com>
2022-03-16 10:54:55 -0700

    is letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,2 +1,22 @@
 #include "main.h"
 
+/**
+ * _isalpha - check main
+ * Description: function for character validation
+ * Return:  1 if c is lowercase or uppercase otherwise return 0
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++){
+		
+		for (upper = 'A'; upper <= 'Z'; upper++){
+
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter);
+}
