bb1e042e0fea66a6d467b371379e6ef122e77261
John Danlami <johnsdanlami@gmail.com>
2022-03-18 21:51:55 -0700

    fixed

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function tha prints if integer is positvie or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
