f2338b4cab79314312cd443e925172c9844b6445
John Danlami <johnsdanlami@gmail.com>
2022-03-21 19:44:35 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _strlen - a function that return the a string length, followed by
+ * a new line, to stdout.
+ * @s: an input string
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while(s[len] != '\0')
+		len++;
+	return (len);
+}
