875570a7eb2c4df894d9433cca525b9131193fbb
John Danlami <johnsdanlami@gmail.com>
2022-03-24 01:06:17 -0700

    null

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(403);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	return (0);
+}
