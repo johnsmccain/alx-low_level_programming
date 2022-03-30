af5d55cd619a26c7c08478935a109a6939872606
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-03-30 18:19:03 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- * wildcmp - Funtion
- *
- * @s1: Variable
- * @s2: Variable
- *
- * Return: Int
+ * wildcmp - A function that compares two strings
+ * @s1: First input string
+ * @s2: Second input string
+ * Return: 1 if strings identical and 0 in otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-
 	if (!*s1 && !*s2)
 		return (1);
 	if (*s1 == *s2)
-		return (wildcmp(s1 +1, s2 + 1));
-	if (*s2 = '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 		return (1);
 	if (*s2 == '*' && *(s1 + 1) && *s2)
 		return (0);
