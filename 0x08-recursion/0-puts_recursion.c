dedee5d969c51876eae76fb2136e7a12e4aca6f1
John Danlami <johnsdanlami@gmail.com>
2022-03-30 02:44:25 -0700

    putchar a function that print a string followed by a new line by recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion(char *s) - function that prints a string follow
+ * by a new line.
+ * @s: input string
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(*s);
+	__puts_recursion(s + 1);
+}
