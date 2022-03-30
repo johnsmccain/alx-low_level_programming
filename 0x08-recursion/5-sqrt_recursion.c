5d13813551000f400f414709ad977e84297d4881
John Danlami <johnsdanlami@gmail.com>
2022-03-30 04:24:03 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,7 +23,7 @@ int _sqrt(int prev, int root)
 {
 	if (prev > root)
 		return (-1);
-	else if (prev * Prev == root)
+	else if (prev * prev == root)
 		return (prev);
 	return (_sqrt(prev + 1, root));
 }
