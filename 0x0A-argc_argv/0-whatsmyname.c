cbbb92758c3af3d09e9eeca21d369a20c47e4ff7
John Danlami <johnsdanlami@gmail.com>
2022-04-01 09:47:19 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - a function that print it file name.
  * @argc: argument count
- * @argv[]: argument value, a string array
+ * @argv: argument value, a string array
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
