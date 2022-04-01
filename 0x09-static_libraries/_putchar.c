46f1890c20fdd6699824859ec41f9b80c49a4b5e
John Danlami <johnsdanlami@gmail.com>
2022-04-01 02:57:12 -0700

    putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 if success.
+ * Ont error, -1 is returned, and the error number is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
