8f48e9061fd8d875fdd4c363598970f3c930b4d8
John Danlami <johnsdanlami@gmail.com>
2022-04-01 09:44:14 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a function that print it file name.
+ * @argc: argument count
+ * @argv[]: argument value, a string array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
