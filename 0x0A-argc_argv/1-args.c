2abf52f94e28c2d520a20df23e9386cc936ae054
John Danlami <johnsdanlami@gmail.com>
2022-04-01 09:55:27 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that print the number of arguments passed into it
+ * @argv: a string array argument
+ * @argc: an int argument
+ * Return: int 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
