2bdd214e166b885012bebd785cd403ff88bc9932
John Danlami <johnsdanlami@gmail.com>
2022-04-01 03:41:08 -0700

    fixedit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
+int _isupper(char c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
