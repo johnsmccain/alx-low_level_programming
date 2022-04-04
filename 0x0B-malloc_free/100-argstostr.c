64ca7985e6f76081f468e4649f0a6bf51d0cb3b5
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-04 20:32:49 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,8 +13,7 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	size = 0;
-	
+	size = 0
 /* count the number of chars in each string */
 	for (i = 0; i < ac; i++)
 	{
@@ -23,8 +22,7 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-	
-/** 
+/**
  * allocate memory for total number of chars and
  * new line for each word
  */
