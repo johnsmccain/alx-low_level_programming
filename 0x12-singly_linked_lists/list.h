0deb88b4cfa3def616c79445b3cb533e33f0715b
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-21 22:50:13 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,7 +1,7 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-#include <stdlib.h>
+#ifndef LIST_H
+#define LIST_H
 
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,15 +13,16 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+  char *str;
+  unsigned int len;
+  struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-size_t list_len_recursion(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif /* _LISTS_H_ */
+int _putchar(char c);
+
+#endif
