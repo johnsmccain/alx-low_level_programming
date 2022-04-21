02e4ac7a692c8cdb4020507f065c5797dccd5ded
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-21 23:18:54 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 
 /**
@@ -23,4 +23,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif /* _LISTS_H_ */
+#endif /* LISTS_H */
