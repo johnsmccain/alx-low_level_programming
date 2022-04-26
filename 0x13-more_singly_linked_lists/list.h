a5f1d577e5794b7035f09aa607a9c27b0d356054
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-26 06:19:47 +0100

    Update list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,19 +1,24 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-size_t recursion_printer(const listint_t *h);
+
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -28,6 +33,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-void free_list(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-#endif /* _LISTS_H_ */
+
+#endif /* LISTS_H */
