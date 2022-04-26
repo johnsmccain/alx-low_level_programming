6adfe623535be2af3444ce93925292c78363b8ab
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-26 06:25:03 +0100

    Update list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,9 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
+#ifndef _LISTS_H_
+#define _LISTS_H_
 #include <stdlib.h>
-#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,8 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-int _putchar(char c);
+size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -33,6 +29,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+void free_list(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif /* LISTS_H */
+#endif /* _LISTS_H_ */
